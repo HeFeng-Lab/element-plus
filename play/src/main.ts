6954cd6ade0345c63c6bae5864e503fb264b7532
hefeng6500 <2443992009@qq.com>
2023-12-18 22:00:43 +0800

    feat: icon 验证

--- play/src/main.ts
+++ play/src/main.ts
@@ -1,5 +1,11 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue"
+import "./style.css"
+import App from "./App.vue"
 
-createApp(App).mount('#app')
+import { ELIcon } from "@code-lab/components"
+
+const app = createApp(App)
+
+app.use(ELIcon)
+
+app.mount("#app")
